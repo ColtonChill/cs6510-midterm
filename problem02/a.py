b9ce84ff0beb52dcb15ebcc7dcca240f6e3ddb9d
jonahboe <boe12005@byui.edu>
2022-09-15 16:14:06 -0600

    Changed inputs to radians

--- problem02/a.py
+++ problem02/a.py
@@ -1,21 +1,24 @@
 from math import cos
 from math import sin
+from math import radians as r
 import numpy as np
 
 a = [[1, 0, 0, 60], 
-     [0, cos(30), -sin(30), 0],
-     [0, sin(30), cos(30), 0],
+     [0, cos(r(30)), -sin(r(30)), 0],
+     [0, sin(r(30)), cos(r(30)), 0],
      [0, 0, 0, 1]]
 
 b = [[1, 0, 0, 40], 
-     [0, cos(45), -sin(45), 0],
-     [0, sin(45), cos(45), 0],
+     [0, cos(r(45)), -sin(r(45)), 0],
+     [0, sin(r(45)), cos(r(45)), 0],
      [0, 0, 0, 1]]
 
 c = [[1, 0, 0, 0], 
-     [0, cos(90), -sin(90), 0],
-     [0, sin(90), cos(90), 0],
+     [0, cos(r(90)), -sin(r(90)), 0],
+     [0, sin(r(90)), cos(r(90)), 0],
      [0, 0, 0, 1]]
 
 z = np.dot(a, b)
+print(z)
+z = np.dot(z, c)
 print(z)
