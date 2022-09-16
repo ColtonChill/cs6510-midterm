e63614ac0dc581ebbfad14318a015ba6279ba524
jonahboe <boe12005@byui.edu>
2022-09-16 09:55:11 -0600

    Fixed a2 dot matrix

--- problem02/a.py
+++ problem02/a.py
@@ -3,19 +3,19 @@ from math import sin
 from math import radians as r
 import numpy as np
 
-a = [[1, 0, 0, 60], 
-     [0, cos(r(30)), -sin(r(30)), 0],
-     [0, sin(r(30)), cos(r(30)), 0],
+a = [[cos(r(30)), -sin(r(30)), 0, 60*cos(r(30))],
+     [sin(r(30)), cos(r(30)), 0, 60*sin(r(30))],
+     [0, 0, 1, 0],
      [0, 0, 0, 1]]
 
-b = [[1, 0, 0, 40], 
-     [0, cos(r(45)), -sin(r(45)), 0],
-     [0, sin(r(45)), cos(r(45)), 0],
+b = [[cos(r(45)), -sin(r(45)), 0, 40*cos(r(45))],
+     [sin(r(45)), cos(r(45)), 0, 40*sin(r(45))],
+     [0, 0, 1, 0],
      [0, 0, 0, 1]]
 
-c = [[1, 0, 0, 0], 
-     [0, cos(r(90)), -sin(r(90)), 0],
-     [0, sin(r(90)), cos(r(90)), 0],
+c = [[cos(r(90)), -sin(r(90)), 0, 0],
+     [sin(r(90)), cos(r(90)), 0, 0],
+     [0, 0, 1, 0],
      [0, 0, 0, 1]]
 
 z = np.dot(a, b)
