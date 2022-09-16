1f910c31d3edefd772c5df88092e7d1ef31f204d
jonahboe <boe12005@byui.edu>
2022-09-16 17:20:19 -0600

    Just proving my math

--- problem02/a.py
+++ problem02/a.py
@@ -18,7 +18,27 @@ c = [[cos(r(90)), -sin(r(90)), 0, 0],
      [0, 0, 1, 0],
      [0, 0, 0, 1]]
 
+d = [[1, 0, 0, 0],
+     [0, 1, 0, 0],
+     [0, 0, 1, 14],
+     [0, 0, 0, 1]]
+
 z = np.dot(a, b)
 print(z)
 z = np.dot(z, c)
 print(z)
+z = np.dot(z, d)
+print(z)
+
+a = 60 * sin(r(30))
+b = 60 * cos(r(30))
+print(a)
+print(b)
+c = 40 * sin(r(75))
+d = 40 * cos(r(75))
+print(c)
+print(d)
+print()
+print("X: ", b+d)
+print("Y: ", a+c)
+print("Z: ", 14)
