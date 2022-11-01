789b1b57366511c834e0e779dc1dac8295a16c2c
jonahboe <boe12005@byui.edu>
2022-10-31 20:51:11 -0600

    Fixed double-writing face data

--- problem06/b.py
+++ problem06/b.py
@@ -24,14 +24,15 @@ while True:
 
     gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
     faces = face_cascade.detectMultiScale(gray_img, 1.25, 4)
-    for (x,y,w,h) in faces: 
+    for i in range(len(faces)): 
+        (x,y,w,h) = faces[i]
         cv2.rectangle(image,(x,y),(x+w,y+h),(255,255,0),2)  
         rec_gray = gray_img[y:y+h, x:x+w] 
         rec_color = image[y:y+h, x:x+w] 
 
         captured_emotions = emotion_detector.detect_emotions(image)
-        for person in captured_emotions:
-            emotions = person['emotions']
+        if len(captured_emotions) == len(faces):
+            emotions = captured_emotions[i]['emotions']
             offset = 20
             for key in emotions.keys():
                 cv2.putText(img=image, text=(str(key) + ": " + str(emotions[key])), org=(x, y+h+offset), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=.5, color=(0, 255, 0),thickness=1)
